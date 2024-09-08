e8a71ff643642d46705c387e0a170a3160ae7d86
Sunitha362 <sunithapeddireddy557558@gmail.com>
2024-09-08 16:17:48 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,62 +1,68 @@
-
 provider "google" {
-    credentials = file("<path-to-your-service-account-key>")
-    project     = "<your-gcp-project-id>"
-    region      = "us-central1"
+  project     = "your-gcp-project-id"
+  region      = "us-central1"
+  credentials = file("path-to-your-service-account.json")
 }
 
+# Create VPC network
 resource "google_compute_network" "vpc_network" {
-    name = "my-vpc"
+  name = "my-vpc-network"
 }
 
+# Public subnet
 resource "google_compute_subnetwork" "public_subnet" {
-    name          = "public-subnet"
-    ip_cidr_range = "10.0.0.0/24"
-    region        = "us-central1"
-    network       = google_compute_network.vpc_network.id
+  name          = "public-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
 }
 
+# Private subnet
 resource "google_compute_subnetwork" "private_subnet" {
-    name          = "private-subnet"
-    ip_cidr_range = "10.0.1.0/24"
-    region        = "us-central1"
-    network       = google_compute_network.vpc_network.id
+  name          = "private-subnet"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
 }
 
+# Compute Engine instance
 resource "google_compute_instance" "web_instance" {
-    name         = "web-instance"
-    machine_type = "e2-medium"
-    zone         = "us-central1-a"
-    tags         = ["http-server"]
+  name         = "web-server"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-11"
-        }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
     }
+  }
 
-    network_interface {
-        network = google_compute_network.vpc_network.name
-        subnetwork = google_compute_subnetwork.public_subnet.name
-        access_config {}
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.public_subnet.id
+    access_config {
+      # This sets up the external IP
     }
+  }
 
-    metadata_startup_script = <<-EOT
-        #!/bin/bash
-        sudo apt-get update
-        sudo apt-get install -y nginx
-    EOT
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+  EOT
 }
 
-resource "google_compute_firewall" "default-allow-http" {
-    name    = "allow-http"
-    network = google_compute_network.vpc_network.name
+# Firewall rule to allow HTTP/HTTPS traffic
+resource "google_compute_firewall" "allow_http_https" {
+  name    = "allow-http-https"
+  network = google_compute_network.vpc_network.id
 
-    allow {
-        protocol = "tcp"
-        ports    = ["80", "443"]
-    }
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
 
-    source_ranges = ["0.0.0.0/0"]
-    target_tags   = ["http-server"]
+  source_ranges = ["0.0.0.0/0"]
 }
